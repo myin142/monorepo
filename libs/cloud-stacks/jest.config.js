bfb6feb9f9e610ab06496873aeed1cfe60ebc05a
Min Yin <minyin142p@gmail.com>
2020-07-01 21:31:41 +0200

    Setup basic travis CI (#1)

--- libs/cloud-stacks/jest.config.js
+++ libs/cloud-stacks/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'cloud-stacks',
-  preset: '../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/cloud-stacks',
+    name: 'cloud-stacks',
+    preset: '../../jest.config.js',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+    coverageDirectory: '../../coverage/libs/cloud-stacks',
 };
