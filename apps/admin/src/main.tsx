c9b49eb70393d475d29ba90288b740ef20df4d63
Min Yin <minyin142p@gmail.com>
2020-07-02 20:12:07 +0200

    [JAP-7] React base url with static hosting redirect

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
         <App />
     </BrowserRouter>,
     document.getElementById('root')
