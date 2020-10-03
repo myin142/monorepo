41bf8cd44500bb6c3bb7095219cfd0803cf35698
myin <minyin142p@gmail.com>
2020-10-03 11:04:11 +0200

    Add cognito callback url

--- apps/aws-app/src/auth-stack.ts
+++ apps/aws-app/src/auth-stack.ts
@@ -3,35 +3,36 @@ import { UserPool, UserPoolClientIdentityProvider, OAuthScope } from '@aws-cdk/a
 import { USERPOOL_DOMAIN } from '../../../libs/shared/interface/src';
 
 export class AuthStack extends Construct {
-    userPool: UserPool;
+	userPool: UserPool;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id);
 
-        const userPoolName = 'MyinUserPool';
-        const userPoolClientName = `${userPoolName}Client`;
-        const userPoolDomainName = `${userPoolName}Domain`;
-        const userPoolDomainPrefix = USERPOOL_DOMAIN;
+		const userPoolName = 'MyinUserPool';
+		const userPoolClientName = `${userPoolName}Client`;
+		const userPoolDomainName = `${userPoolName}Domain`;
+		const userPoolDomainPrefix = USERPOOL_DOMAIN;
 
-        this.userPool = new UserPool(this, 'userpool', { userPoolName });
+		this.userPool = new UserPool(this, 'userpool', { userPoolName });
 
-        this.userPool.addClient(userPoolClientName, {
-            supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
-            oAuth: {
-                flows: { implicitCodeGrant: true },
-                scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.PROFILE],
-                callbackUrls: [
-                    'http://localhost:8080/login',
-                    'http://localhost:4200/login',
-                    'http://localhost:4300/login',
-                    'https://myin142.github.io/japanese/login',
-                    'https://myin142.github.io/admin/login',
-                ],
-            },
-        });
+		this.userPool.addClient(userPoolClientName, {
+			supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
+			oAuth: {
+				flows: { implicitCodeGrant: true },
+				scopes: [OAuthScope.EMAIL, OAuthScope.OPENID, OAuthScope.PROFILE],
+				callbackUrls: [
+					'http://localhost:8080/login',
+					'http://localhost:4200/login',
+					'http://localhost:4300/login',
+					'https://myin142.github.io/japanese/login',
+					'https://myin142.github.io/admin/login',
+					'https://myin142.github.io/myin-users/login',
+				],
+			},
+		});
 
-        this.userPool.addDomain(userPoolDomainName, {
-            cognitoDomain: { domainPrefix: userPoolDomainPrefix },
-        });
-    }
+		this.userPool.addDomain(userPoolDomainName, {
+			cognitoDomain: { domainPrefix: userPoolDomainPrefix },
+		});
+	}
 }
